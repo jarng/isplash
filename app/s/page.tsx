b9c9efe18b49109012f4f417ca501eece77316f5
Hieu Nguyen <nguyenhotrunghieu0106@gmail.com>
2023-08-04 12:22:49 +0700

    feature: add searched images

--- app/s/page.tsx
+++ app/s/page.tsx
@@ -1,18 +1,24 @@
 'use client';
 
 import { useSearchParams } from 'next/navigation'
+import { useSearchPhotos } from '@/lib/api';
+import ImageGallery from '@/components/ImageGallery';
 
 type Props = {}
 
-function SearchPage({}: Props) {
+function SearchPage({ }: Props) {
   const searchParams = useSearchParams();
-  const search = searchParams.get('query');
+  const search = searchParams.get('query') || '';
   console.log(search);
 
+  const { data, isError, isLoading } = useSearchPhotos(30, search);
+  console.log(data);
+
+
   return (
-    <div>
-      Hello world
-    </div>
+    <main className='w-full'>
+      <ImageGallery images={data} />
+    </main>
   )
 }
 
--- lib/api.ts
+++ lib/api.ts
@@ -1,3 +1,5 @@
+import useSWR from 'swr';
+
 const PUBLIC_KEY = process.env.PUBLIC_KEY;
 
 export const getPhotos = async (page: number, topicSlug?: string): Promise<TImage[]> => {
@@ -49,4 +51,40 @@ export const getTopics = async (): Promise<TTopic[]> => {
     title: topic.title,
     description: topic.description,
   }));
+}
+
+const explicitKey = 'wc1Xg-SiEPLbjNAGrRXfBvvoXtnLFKNvnH6BgkHr3Pg'
+const fetcher = (url: string): Promise<TImage[]> => fetch(url).then((r: any) => {
+  if (!r.ok) {
+    throw new Error(r.statusText);
+  }
+
+  return r.json().then((data: any) => (
+    data.results.map((image: any) => ({
+      id: image.id,
+      alt: image.alt_description,
+      src: image.urls.regular,
+      width: image.width,
+      height: image.height,
+    }))
+  ));
+})
+
+export const useSearchPhotos = (page: number, search: string): {
+  data: TImage[],
+  isError: boolean,
+  isLoading: boolean,
+} => {
+  const url = `https://api.unsplash.com/search/photos/?query=${search}&client_id=${explicitKey}&per_page=${page}`;
+  console.log('url', url);
+  const { data, error, isLoading } = useSWR(
+    url,
+    fetcher,
+  )
+
+  return {
+    data: data || [],
+    isError: error,
+    isLoading: isLoading,
+  }
 }
