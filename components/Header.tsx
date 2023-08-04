cbac2865bd2802fece03f9efafc4b5a4aa615e93
Hieu Nguyen <nguyenhotrunghieu0106@gmail.com>
2023-08-04 13:10:38 +0700

    feature: add current path css

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,16 @@
+import { getTopics } from "@/lib/api";
 import NavBar from "./NavBar"
 import SearchBar from "./SearchBar"
 
 type Props = {}
 
-function Header({ }: Props) {
+async function Header({ }: Props) {
+  const topics = await getTopics();
+
   return (
     <div className='flex px-6 items-center justify-between fixed top-0 border-b-2 border-solid bg-white shadow-gray-50 w-full h-16'>
       <SearchBar />
-      <NavBar />
+      <NavBar topics={topics}/>
     </div>
   )
 }
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,23 +1,27 @@
-import { getTopics } from '@/lib/api';
+'use client';
+
+import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 
 type Props = {
-  className?: string
+  className?: string;
+  topics: TTopic[]
 }
 
-async function NavBar({ className = '' }: Props) {
-  const topics = await getTopics();
+function NavBar({ className = '', topics }: Props) {
+  const url = usePathname();
+  console.log(url);
 
   return (
     <nav className={`${className} w-full`}>
       <div className='flex max-w-3xl h-full mx-auto items-center justify-between'>
-        <Link href='/' className='text-gray-500 hover:text-gray-900'>Home</Link>
+        <Link href='/' className={`text-gray-500 hover:text-gray-900 ${url === '/' && 'underline text-inherit'}`}>Home</Link>
         {
           topics.map((topic) => (
             <div key={topic.id}>
               <Link
                 href={`/t/${topic.slug}`}
-                className='text-gray-500 hover:text-gray-900'
+                className={`text-gray-500 hover:text-gray-900 ${url === `/t/${topic.slug}` && 'underline text-inherit'}`}
               >
                 {topic.title}
               </Link>
